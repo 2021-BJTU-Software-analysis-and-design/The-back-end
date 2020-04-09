a218a5ceee97323eb7ced60da3ccaaa37827f0d5
b5ck.work@gmail.com <b5ck.work@gmail.com>
2020-04-09 23:28:23 +0800

    day08: 初始化 test-fastdfs 工程

---
+++ test-fastdfs/src/main/java/com/xuecheng/test/fastdfs/TestFastDfsApplication.java
@@ -0,0 +1,11 @@
+package com.xuecheng.test.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFastDfsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestFastDfsApplication.class,args);
+    }
+}
---
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -0,0 +1,12 @@
+package com.xuecheng.test.fastdfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestFastDFS {
+
+}
