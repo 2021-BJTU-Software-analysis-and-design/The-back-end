fb007bc57a3225620c1874911bf0b3e512ef7456
b5ck.work@gmail.com <b5ck.work@gmail.com>
2020-04-16 09:03:06 +0800

    day10: cms client 服务注册到eureka

--- xc-service-manage-cms-client/src/main/java/com/xuecheng/manage_cms_client/ManageCmsClientApplication.java
+++ xc-service-manage-cms-client/src/main/java/com/xuecheng/manage_cms_client/ManageCmsClientApplication.java
@@ -2,8 +2,10 @@ package com.xuecheng.manage_cms_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages={"com.xuecheng.framework"})//扫描common下的所有类
 @ComponentScan(basePackages={"com.xuecheng.manage_cms_client"})
